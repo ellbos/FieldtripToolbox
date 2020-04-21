af71a1fc28a3ad9ddc809e11bcaeb4d37dad72b4
ellbos <e.bosch@donders.ru.nl>
2020-04-21 11:40:49 +0200

    Added time-frequency analysis

--- day2.m
+++ day2.m
@@ -33,3 +33,22 @@ dataFIC = ft_redefinetrial(cfg, data_all);
 
 % save
 save dataFIC dataFIC
+
+%% Time-frequency analysis
+% Hanning taper, fixed window length
+
+%{
+load dataFIC
+%} 
+
+% specify time-frequency analysis specs
+cfg              = [];
+cfg.output       = 'pow';
+cfg.channel      = 'MEG';
+cfg.method       = 'mtmconvol';
+cfg.taper        = 'hanning';
+cfg.foi          = 2:2:30;                         % analysis 2 to 30 Hz in steps of 2 Hz
+cfg.t_ftimwin    = ones(length(cfg.foi),1).*0.5;   % length of time window = 0.5 sec
+cfg.toi          = -0.5:0.05:1.5;                  % time window "slides" from -0.5 to 1.5 sec in steps of 0.05 sec (50 ms)
+TFRhann = ft_freqanalysis(cfg, dataFIC);
+
