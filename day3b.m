30d47352ee7b2981577bcfb50bd1d0c1be795e8d
ellbos <e.bosch@donders.ru.nl>
2020-04-22 15:06:23 +0200

    Added timelock statistic

--- day3b.m
+++ day3b.m
@@ -51,3 +51,38 @@ cfg.keeptrials = 'yes';
 timelockFIC = ft_timelockanalysis(cfg, dataFIC_LP);
 timelockFC  = ft_timelockanalysis(cfg, dataFC_LP);
 
+%% Timelock statistic
+
+% configuration
+cfg = [];
+cfg.method              = 'montecarlo'; % use the Monte Carlo Method to calculate the significance probability
+cfg.statistic           = 'ft_statfun_indepsamplesT'; % use the independent samples T-statistic as a measure to evaluate the effect at the sample level
+cfg.correctm            = 'cluster';
+cfg.clusteralpha        = 0.05;     % alpha level of the sample-specific test statistic that will be used for thresholding
+cfg.clusterstatistic    = 'maxsum'; % test statistic that will be evaluated under the permutation distribution.
+cfg.minnbchan           = 2;        % minimum number of neighborhood channels that is required for a selected sample to be included in the clustering algorithm (default=0).
+% cfg.neighbours = neighbours;      % see below
+cfg.tail                = 0;        % -1, 1 or 0 (default = 0); one-sided or two-sided test
+cfg.clustertail         = 0;
+cfg.alpha               = 0.025;    % alpha level of the permutation test
+cfg.numrandomization    = 100;      % number of draws from the permutation distribution
+
+design = zeros(1,size(timelockFIC.trial,1) + size(timelockFC.trial,1));
+design(1,1:size(timelockFIC.trial,1)) = 1;
+design(1,(size(timelockFIC.trial,1)+1):(size(timelockFIC.trial,1) + size(timelockFC.trial,1)))= 2;
+
+cfg.design              = design;   % design matrix
+cfg.ivar                = 1;        % number or list with indices indicating the independent variable(s)
+
+cfg_neighb              = [];
+cfg_neighb.method       = 'distance';
+neighbours              = ft_prepare_neighbours(cfg_neighb, dataFC_LP);
+
+cfg.neighbours          = neighbours;  % the neighbours specify for each sensor with which other sensors it can form clusters
+cfg.channel             = {'MEG'};     % cell-array with selected channel labels
+cfg.latency             = [0 1];       % time interval over which the experimental conditions must be compared (in seconds)
+
+% statistic
+[stat] = ft_timelockstatistics(cfg, timelockFIC, timelockFC);
+
+% save the output
