73148ae936fdf0d4f517e06dcb6bd01a5526968a
ellbos <e.bosch@donders.ru.nl>
2020-04-23 11:31:59 +0200

    Added simulation of data with directed connections

---
+++ day4.m
@@ -0,0 +1,38 @@
+%% Simulate data with directed connections
+
+rng('default')
+rng(50) %arbitrarily chosen random number generator for reproducibility
+
+% configuration
+cfg             = [];
+cfg.ntrials     = 500;
+cfg.triallength = 1;
+cfg.fsample     = 200;
+cfg.nsignal     = 3;
+cfg.method      = 'ar';
+
+% NxN coefficient matrices
+cfg.params(:,:,1) = [ 0.8    0    0 ;
+                        0  0.9  0.5 ;
+                      0.4    0  0.5];
+
+cfg.params(:,:,2) = [-0.5    0    0 ;
+                        0 -0.8    0 ;
+                        0    0 -0.2];
+
+cfg.noisecov      = [ 0.3    0    0 ;
+                        0    1    0 ;
+                        0    0  0.2];
+
+data              = ft_connectivitysimulation(cfg);
+
+% plot the data
+figure
+plot(data.time{1}, data.trial{1})
+legend(data.label)
+xlabel('time (s)')
+
+% browse through the data
+cfg = [];
+cfg.viewmode = 'vertical';  % you can also specify 'butterfly'
+ft_databrowser(cfg, data);
