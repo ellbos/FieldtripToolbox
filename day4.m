b6b7cebae4b4229cf4544464c96ba2241c257f18
ellbos <e.bosch@donders.ru.nl>
2020-04-23 12:33:53 +0200

    Added simulated data with common pick-up and different noise levels

--- day4.m
+++ day4.m
@@ -88,3 +88,62 @@ for col=1:3
 end
 end
 
+%% Simulated data with common pick-up and different noise levels
+
+% create some instantaneously mixed data
+
+% define some variables locally
+nTrials  = 100;
+nSamples = 1000;
+fsample  = 1000;
+
+% mixing matrix
+mixing   = [0.8 0.2 0;
+              0 0.2 0.8];
+
+data       = [];
+data.trial = cell(1,nTrials);
+data.time  = cell(1,nTrials);
+for k = 1:nTrials
+  dat = randn(3, nSamples);
+  dat(2,:) = ft_preproc_bandpassfilter(dat(2,:), 1000, [15 25]);
+  dat = 0.2.*(dat-repmat(mean(dat,2),[1 nSamples]))./repmat(std(dat,[],2),[1 nSamples]);
+  data.trial{k} = mixing * dat;
+  data.time{k}  = (0:nSamples-1)./fsample;
+end
+data.label = {'chan1' 'chan2'}';
+
+figure;plot(dat'+repmat([0 1 2],[nSamples 1]));
+title('original ''sources''');
+
+figure;plot((mixing*dat)'+repmat([0 1],[nSamples 1]));
+axis([0 1000 -1 2]);
+set(findobj(gcf,'color',[0 0.5 0]), 'color', [1 0 0]);
+title('mixed ''sources''');
+
+% do spectral analysis
+cfg = [];
+cfg.method    = 'mtmfft';
+cfg.output    = 'fourier';
+cfg.foilim    = [0 200];
+cfg.tapsmofrq = 5;
+freq          = ft_freqanalysis(cfg, data);
+fd            = ft_freqdescriptives(cfg, freq);
+
+figure;plot(fd.freq, fd.powspctrm);
+set(findobj(gcf,'color',[0 0.5 0]), 'color', [1 0 0]);
+title('power spectrum');
+
+% compute connectivity
+cfg = [];
+cfg.method = 'granger';
+g = ft_connectivityanalysis(cfg, freq);
+cfg.method = 'coh';
+c = ft_connectivityanalysis(cfg, freq);
+
+% visualize the results
+cfg = [];
+cfg.parameter = 'grangerspctrm';
+figure; ft_connectivityplot(cfg, g);
+cfg.parameter = 'cohspctrm';
+figure; ft_connectivityplot(cfg, c);
